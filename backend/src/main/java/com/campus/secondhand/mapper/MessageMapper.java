fd54eaf6324631dc22b9e1e5cd7ccfcf920876e7
lenovo <1191992723@qq.com>
2026-09-17 17:26:43 +0800

    feat：添加评论和分页查询评论

---
+++ backend/src/main/java/com/campus/secondhand/mapper/MessageMapper.java
@@ -0,0 +1,18 @@
+package com.campus.secondhand.mapper;
+
+import com.campus.secondhand.pojo.Message;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author lenovo
+* @description 针对表【message(站内消息)】的数据库操作Mapper
+* @createDate 2026-09-16 11:56:08
+* @Entity com.campus.secondhand.entity.Message
+*/
+public interface MessageMapper extends BaseMapper<Message> {
+
+}
+
+
+
+
