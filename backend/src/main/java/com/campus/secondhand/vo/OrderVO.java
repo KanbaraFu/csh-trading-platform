9d00c67351d42b977080903c7f3dcb5ee1c3d461
神原 風 <1173906299@qq.com>
2026-09-21 04:57:51 +0800

    fix:修正OrderVO的avatar描述为头像

--- backend/src/main/java/com/campus/secondhand/vo/OrderVO.java
+++ backend/src/main/java/com/campus/secondhand/vo/OrderVO.java
@@ -48,11 +48,11 @@ public class OrderVO {
 
     private String buyerNickname;       // 买家
 
-    private String buyerAvatar;         // 买家地址
+    private String buyerAvatar;         // 买家头像
 
     private String sellerNickname;      // 卖家
 
-    private String sellerAvatar;        // 卖家地址
+    private String sellerAvatar;        // 卖家头像
 
     private List<OrderItemVO> items;      // 订单的每个商品详情
 
