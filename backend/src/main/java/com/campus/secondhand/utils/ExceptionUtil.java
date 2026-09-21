9a0c92eb7de070f620856bbbd67c7681a071dfe4
神原 風 <1173906299@qq.com>
2026-09-21 12:02:59 +0800

    feat:更新了异常工具类的方法

--- backend/src/main/java/com/campus/secondhand/utils/ExceptionUtil.java
+++ backend/src/main/java/com/campus/secondhand/utils/ExceptionUtil.java
@@ -9,4 +9,32 @@ public class ExceptionUtil {
             throw  new BizException(msg);
         }
     }
+
+    /* 是否请求失败 */
+    public static void isBadRequest(Boolean flag,String msg) {
+        if(flag){
+            throw BizException.badRequest(msg);
+        }
+    }
+
+    /* 是否处于登录状态 */
+    public static void isUnAuthorized(Boolean flag,String msg) {
+        if(flag){
+            throw BizException.unauthorized(msg);
+        }
+    }
+
+    /* 是否访问了未授权的资源 */
+    public static void isForbidden(Boolean flag,String msg) {
+        if(flag){
+            throw BizException.forbidden(msg);
+        }
+    }
+
+    /* 是否未找到页面 */
+    public static void isNotFound(Boolean flag,String msg) {
+        if(flag){
+            throw BizException.notFound(msg);
+        }
+    }
 }
